9709464032224753966cfe411b453234d60cb246
TOHID KHAN <tohidkhan1193407@gmail.com>
2025-09-07 16:24:27 +0530

    fix: Use server_class for server instantiation in start.py

--- start.py
+++ start.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         port = int(os.environ.get("PORT", 8010))
         print(f"🚀 Starting server on port {port}...")
         config = Config(app="app.main:app", host="0.0.0.0", port=port, reload=False)
-        server = ProactorServer(config=config)
+        server = server_class(config=config)
         server.run()
         print('✅ Server Started Successfully.')
 
